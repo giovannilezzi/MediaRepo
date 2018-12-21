e1e5264c53e56257df53c0575c73137b0a6cf024
Michele225 <michele.tar22@gmail.com>
2018-12-21 12:45:47 +0100

    Aggiornamento libreria multimediale

--- src/Components/ListFile.js
+++ src/Components/ListFile.js
@@ -14,31 +14,23 @@ class ListFile extends React.Component{
     }
 
     viewFile = () => {
+
         var that = this
-        $('li.image').on('click', function (e) {
+        $('a.tile').on('click', function (e) {
             e.preventDefault();
             var id = $(this).attr('id');
-            console.log(id)
-            console.log(that.props.listFiles)
-            console.log(that.props.listFiles[id])
             that.props.AsyncCallGetFileById(id)
             //that.props.saveFile(that.props.listFiles[id-1])
         });
         $('li.pdf').on('click', function (e) {
             e.preventDefault();
             var id = $(this).attr('id');
-            console.log(id)
-            console.log(that.props.listFiles)
-            console.log(that.props.listFiles[id])
             that.props.AsyncCallGetFileById(id)
             //that.props.saveFile(that.props.listFiles[id-1])
         });
         $('li.file').on('click', function (e) {
             e.preventDefault();
             var id = $(this).attr('id');
-            console.log(id)
-            console.log(that.props.listFiles)
-            console.log(that.props.listFiles[id])
             that.props.AsyncCallGetFileById(id)
             //that.props.saveFile(that.props.listFiles[id-1])
         });
@@ -49,38 +41,67 @@ class ListFile extends React.Component{
         if (this.props.listFiles && !this.props.isLoading) {
             for(var i = 0; i<this.props.listFiles.length; i++){
                 if(this.props.listFiles[i].MimeType == 'data:image/png;base64,' || this.props.listFiles[i].MimeType == 'data:image/jpeg;base64,') {
-                    listImage.push(
+                 /*   listImage.push(
                         <li className="menuover" id={this.props.listFiles[i].Id} onClick={this.viewFile} className="image" key={this.props.listFiles[i].Id} >
                             {
                                 <a className="far fa-file-image fa-9x link " > {this.props.listFiles[i].Name} </a>
                             }
                         </li>
+                    )*/
+                    listImage.push(
+                    <a className="tile" id={this.props.listFiles[i].Id} onClick={this.viewFile}  key={this.props.listFiles[i].Id}>
+                        <h2 className="tile-description">{this.props.listFiles[i].Name}</h2>
+                        <div className="tile-divider"></div>
+                        <p className=" far fa-file-image fa-3x"> </p>
+                    </a>
                     )
+
                 }
                 else if(this.props.listFiles[i].MimeType == 'data:application/pdf;base64,' ) {
-                    listImage.push(
+                  /*  listImage.push(
                         <li className="menuover" id={this.props.listFiles[i].Id} onClick={this.viewFile} className="pdf" key={this.props.listFiles[i].Id} >
                             {
                                 <a className=" far fa-file-pdf fa-9x link" > {this.props.listFiles[i].Name} </a>
                             }
                         </li>
+                    )*/
+                    listImage.push(
+                        <a className="tile" id={this.props.listFiles[i].Id} onClick={this.viewFile}  key={this.props.listFiles[i].Id}>
+                            <h2 className="tile-description">{this.props.listFiles[i].Name}</h2>
+                            <div className="tile-divider"></div>
+                            <p className=" far fa-file-pdf fa-3x"> </p>
+                        </a>
                     )
                 }
                 else if(this.props.listFiles[i].MimeType == 'data:text/plain;base64,' ) {
-                    listImage.push(
+                  /*  listImage.push(
                         <li className="menuover" id={this.props.listFiles[i].Id} onClick={this.viewFile} className="file" key={this.props.listFiles[i].Id} >
                             {
                                 <a className="far fa-file-alt fa-9x link" > {this.props.listFiles[i].Name} </a>
                             }
                         </li>
+                    )*/
+                    listImage.push(
+                        <a className="tile" id={this.props.listFiles[i].Id} onClick={this.viewFile}  key={this.props.listFiles[i].Id}>
+                            <h2 className="tile-description">{this.props.listFiles[i].Name}</h2>
+                            <div className="tile-divider"></div>
+                            <p className=" far fa-file-alt fa-3x"> </p>
+                        </a>
                     )
                 }else {
-                    listImage.push(
+                   /* listImage.push(
                         <li className="menuover" id={this.props.listFiles[i].Id} onClick={this.viewFile} className="file" key={this.props.listFiles[i].Id} >
                             {
                                 <a className="far fa-file-alt fa-9x link" > {this.props.listFiles[i].Name} </a>
                             }
                         </li>
+                    )*/
+                    listImage.push(
+                        <a className="tile" id={this.props.listFiles[i].Id} onClick={this.viewFile}  key={this.props.listFiles[i].Id}>
+                            <h2 className="tile-description">{this.props.listFiles[i].Name}</h2>
+                            <div className="tile-divider"></div>
+                            <p className=" far fa-file-alt fa-3x"> </p>
+                        </a>
                     )
                 }
 
@@ -97,8 +118,9 @@ class ListFile extends React.Component{
             <div>
                 <div>
                     <h1 className="titolo">Libreria Multimediale</h1></div>
-                 {listImage}
-
+                    <div className="tiles-flex">
+                         {listImage}
+                     </div>
             </div>
         )
     }
